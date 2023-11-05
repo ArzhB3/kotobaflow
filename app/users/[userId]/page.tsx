dff6c4efe8a61df7f7ad77806116eb56f248bb75
Arthur Pagelot <arthur.pagelot@gmail.com>
2023-11-05 19:28:54 +0100

    Add Follow/Unfollow Button & Actions

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,7 +1,12 @@
 import { getAuthSession } from '@/lib/auth';
 import { getUserProfile } from '@/src/query/user.query';
 import { Profile } from './profile';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
+import { prisma } from '@/lib/prisma';
+import { Button } from '@/components/ui/button';
+import { followUser } from './follow.action';
+import { is } from 'date-fns/locale';
+import { Post } from '@/src/feature/post/post';
 
 export default async function UserPage({
 	params,
@@ -15,9 +20,50 @@ export default async function UserPage({
 		notFound();
 	}
 
+	const isFollower = session?.user.id
+		? await prisma.follow.findFirst({
+				where: {
+					followerId: session?.user?.id,
+					followingId: user.id,
+				},
+				select: {
+					id: true,
+				},
+		  })
+		: false;
+
+	const isCurrentUser = params.userId === session?.user.id;
+
+	if (isCurrentUser) {
+		redirect('/profile');
+	}
+
 	return (
 		<div>
-			<Profile user={user} />
+			<Profile user={user}>
+				<form className='mt-4'>
+					<Button
+						variant='outline'
+						formAction={async () => {
+							'use server';
+							if (!session?.user.id) {
+								return;
+							}
+							await followUser(params.userId);
+						}}
+					>
+						{isFollower ? 'Unfollow' : 'Follow'}
+					</Button>
+				</form>
+			</Profile>
+			<div className='divide-y divide-accent border-t border-accent mt-4'>
+				{user.posts.map((post) => (
+					<Post
+						key={post.id}
+						post={post}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
