c8eb4a8e72ce520c8eceb9d9d80102d0471b5d4d
Arthur Pagelot <arthur.pagelot@gmail.com>
2023-11-05 21:40:42 +0100

    Add Edit Action & Form To Edit Profile Page

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,3 +1,18 @@
-export const EditPage = () => {
-	return <div>EditPage</div>;
-};
+import { getUserEdit } from '@/src/query/user.query';
+import { ProfileForm } from './profileForm';
+import { editProfile } from './edit-profile.action';
+
+export default async function EditPage() {
+	const user = await getUserEdit();
+
+	return (
+		<div className='h-full container flex items-center'>
+			<div className='bg-card border rounded-md border-border p-4 flex-1'>
+				<ProfileForm
+					user={user}
+					onSubmit={editProfile}
+				/>
+			</div>
+		</div>
+	);
+}
